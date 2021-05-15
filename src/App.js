9a3db491f8d5f9fc14d3897c1747f433ca0bc87b
kianoosh <kianooshnike1365@gmail.com>
2021-05-15 15:51:58 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   return (
     <div className="App">
       <Wrapper>
-        <Title>Hello World!</Title>
+        <Title>Hi World!</Title>
       </Wrapper>
       <Container>
         <Button>Normal Button</Button>
